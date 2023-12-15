f023bf7a507ac1071c1a7972abc8dfc4b5877824
Rashida-3 <rashidaansari0123@gmail.com>
2023-12-15 14:54:44 +0530

    module

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { SignInComponent } from './user-auth/sign-in/sign-in.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { CountryListComponent } from './country-list/country-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    SignInComponent,
+    UserListComponent,
+    StudentListComponent,
+    CountryListComponent
   ],
   imports: [
     BrowserModule
